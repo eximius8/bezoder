25dcb8974eb18fb516c473f4ed5503afcc626082
Mikhail <mikhail.trunov@gmail.com>
2020-03-11 01:21:41 +0400

    Visitor and Creator classes

--- person/models.py
+++ person/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class User(AbstractUser):
     pass
+
+
+class Visitor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField(blank=True)
+    # in cm
+    height = models.FloatField(blank=True, validators=[MinValueValidator(5.), MaxValueValidator(500)])
+    opensnpid = models.PositiveIntegerField(blank=True)
+
+class Creator(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
