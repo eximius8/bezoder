2192eee65e816f4bc982283241d44917057a0802
Mikhail <mikhail.trunov@gmail.com>
2020-03-11 15:45:47 +0400

    Latest commit for PC

--- person/models.py
+++ person/models.py
@@ -12,6 +12,7 @@ class Visitor(models.Model):
     # in cm
     height = models.FloatField(blank=True, validators=[MinValueValidator(5.), MaxValueValidator(500)])
     opensnpid = models.PositiveIntegerField(blank=True)
+    sex = models.
 
 class Creator(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
